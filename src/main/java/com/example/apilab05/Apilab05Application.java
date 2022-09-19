5a3323a359d7329de5ba4b5d317fda6cb9f7bad2
SMinhChau <minhchaunguyenmtp@gmail.com>
2022-09-19 07:02:35 +0700

    create project

---
+++ src/main/java/com/example/apilab05/Apilab05Application.java
@@ -0,0 +1,13 @@
+package com.example.apilab05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apilab05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apilab05Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apilab05/Apilab05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apilab05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apilab05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
